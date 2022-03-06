566f0ed2431c5f1fc07cfa139bdcc1b88b71d218
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2022-03-06 11:28:37 +0500

    Es6-for-react

---
+++ app.js
@@ -0,0 +1,320 @@
+// import {person} from './customer';
+
+// console.log(person());
+
+
+
+
+
+
+// // let and const react me var use nahi krengy let r const use krengy
+
+// // let myname="sharjeel";
+
+// //  myname="tanzil"
+
+// //  console.log(myname)
+
+
+// // const myname="sharjeel";
+// // // const me vlaue change nahi hoti he let me change hojati heee 
+
+// // //  myname="tanzil"
+
+// //  console.log(myname)
+
+
+
+// // arrow function
+
+// // let names=(names) => {
+// //     console.log(names)
+// // }
+
+// // names("heelo world");
+
+// // agr apka argument aik he jb ap parameter ki brakets hata sakte hu agr 0 ho tb bh brakets lgana he agr 2 ho tb bh bss aik per agr dil kre tw na lgao
+// // let names=names=> {
+// //     console.log(names)
+// // }
+
+// // names("heelo world");
+
+// // // agr retrun karwana he tw function ke bahar console kro func name ke sath 
+// // let names=names=> {
+// //     return names    
+// // }
+// // console.log(names("heelo world"));
+
+
+// //agr return he tw carle brakets hata sakte hein r istrha form banega
+
+// // let names=names=>names    
+// // console.log(names("heelo world"));
+
+// // const alpha=value=>value
+// // console.log(alpha("herjeet"))
+
+// // const gamma=xyz=>xyz
+// // console.log(gamma("jeet"))
+
+
+// // const beta=abc=>abc
+// // console.log(beta("premjeet")) 
+
+// // let hello=
+// // say=>say;
+// // console.log(hello("world"))
+
+
+// class Customer{
+//     constructor(n){
+//         this.number=n;
+//     }
+//     buy(){
+//         console.log("Hello................")
+//     }
+// }
+
+// let customer1=new Customer("Fazeel");
+
+// console.log(customer1);
+
+
+// customer1.buy();
+
+// let customer2=new Customer("Tanzil");
+
+// console.log(customer2);
+
+// customer2.buy();
+
+//  let customer3 = new Customer("Sharjeel");
+
+//  console.log(customer3);
+
+//  customer3.buy();
+
+
+
+//     class Customer{
+//         constructor(n){
+//             this.name=n;
+//         }
+
+//         buy(){
+//             console.log("Hello.....................")
+//         }
+
+//     }
+
+
+// let customer1=new Customer("Shazaib");
+
+// console.log(customer1);
+
+// customer1.buy();
+ 
+// let customer2=new Customer ("Hashim-bhai")
+// console.log(customer2);
+
+// customer2.buy();
+
+
+
+
+// class Trends{
+//     constructor(n){
+//         this.items=n;
+//     }
+//     buy(){
+//         console.log("Chicken Biryani")
+//     }
+// }
+
+// let customer=new Trends("Beef");
+// console.log(customer);
+
+// customer.buy();
+
+
+
+
+//class inheritance section
+// class Customer{
+//     constructor(n){
+//         this.number=n;
+//     }
+//     buy(){
+//         console.log("Hello................")
+//     }
+// }
+
+// class Newcustomers extends Customer{
+
+//     hello(){
+//         console.log("Hello World")
+//     }
+// }
+
+// let customer=new Newcustomers("Fazeel")
+// console.log(customer)
+
+// customer.buy();
+// customer.hello();
+
+
+
+
+// class Customer{
+//     constructor(n){
+//         this.number=n;
+//     }
+//     buy(){
+//         console.log("Hello................")
+//     }
+// }
+
+// class Newcustomers extends Customer{
+//         age=24;
+//     hello=()=>{
+//         console.log("Hello World")
+//     }
+// }
+
+// let customer=new Newcustomers("Fazeel")
+// console.log(customer)
+
+
+
+// class Customer{
+//     constructor(n){
+//         this.number=n;
+//     }
+//     buy(){
+//         console.log("Hello................")
+//     }
+// }
+
+// class Newcustomers extends Customer{
+//         age=24;
+//     hello=()=>{
+//         console.log("Hello World")
+//     }
+// }
+
+// let customer=new Newcustomers("Fazeel")
+// console.log(customer)
+
+
+
+// // Array desturcturing
+
+
+// let list=["Jaguar","Audi","BMW","Carollaa"];
+
+// // console.log(list);
+
+// let [car1,car2,car3,car4]=list
+
+// // console.log(...list)
+// console.log(car4)
+
+
+
+// Object Destructure
+
+
+// const person={
+//     name:"Hellow World",
+//     age:2899,
+// }
+
+// let {name,age}=person
+
+// console.log(person.age)
+
+
+// const person={
+//     name:"Hellow World",
+//     age:2899,
+// }
+
+// let {name,age}=person
+
+// console.log(name)
+
+
+//Refrence array object and primitive type string number
+
+
+// let num1 =100;
+// let num2=200;
+// let num3=num1+num2;
+// console.log(num3)
+
+
+// // this is not copying this is refernce only
+// let person={
+//     name:"Faizan"
+// }
+
+// let person2=person;
+// console.log(person);
+// console.log(person2);
+
+// this is copy 
+// let person={
+//     name:"Faizan"
+// }
+
+// let person2={
+//     ...person,
+//     name:"ajeet"
+// }
+// console.log(person);
+// console.log(person2);
+
+
+// map  this is sample array with map  
+
+// let arr1=[2,3,4,5,6];
+
+// let arr2=arr1.map(function(x){
+//         return x*2;
+// })
+
+// console.log(arr2);
+
+
+// Now its map is update 
+
+// let arr1=[2,3,4,5,6];
+
+// let arr2=arr1.map(x=>
+//      x*2
+// )
+
+// console.log(arr2);
+
+
+// let arr1=[2,3,4,5,6];
+
+// let arr2=arr1.map(x=>
+//      x*3
+// )
+
+// console.log(arr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ customer.js
@@ -0,0 +1,11 @@
+
+// const person={
+//     myname:"Vermaa-khan"
+//     // address:"Orangi town Karachi"
+// }
+
+export const person=()=>{
+    console.log("World")
+}
+
+person();
